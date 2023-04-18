17d2b3cf4018662fe4d637c350f875e09a14aace
Marcel Richter <mail@mrclrchtr.de>
2023-04-18 13:26:39 +0200

    Add possibility to start main.py with config.yml

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 """ Startup file for Google Cloud deployment or local webserver"""
+import argparse
 import os
 
 from flathunter.idmaintainer import IdMaintainer
 from flathunter.googlecloud_idmaintainer import GoogleCloudIdMaintainer
 from flathunter.web_hunter import WebHunter
-from flathunter.config import Config
+from flathunter.config import Config, Env
 from flathunter.logging import configure_logging
 
 from flathunter.web import app
 
-config = Config()
+parser = argparse.ArgumentParser(
+    description=("Searches for flats on Immobilienscout24.de and wg-gesucht.de"
+                 " and sends results to Telegram User"),
+    epilog="Designed by Nody"
+)
+if Env.FLATHUNTER_TARGET_URLS is not None:
+    default_config_path = None
+else:
+    default_config_path = f"{os.path.dirname(os.path.abspath(__file__))}/config.yaml"
+parser.add_argument('--config', '-c',
+                    type=argparse.FileType('r', encoding='UTF-8'),
+                    default=default_config_path,
+                    help=f'Config file to use. If not set, try to use "{default_config_path}"'
+                    )
+parser.add_argument('--heartbeat', '-hb',
+                    action='store',
+                    default=None,
+                    help=('Set the interval time to receive heartbeat messages to check'
+                          'that the bot is alive. Accepted strings are "hour", "day", "week".'
+                          'Defaults to None.')
+                    )
+args = parser.parse_args()
+
+# load config
+config_handle = args.config
+if config_handle is not None:
+    config = Config(config_handle.name)
+else:
+    config = Config()
 
 if __name__ == '__main__':
     # Use the SQLite DB file if we are running locally
